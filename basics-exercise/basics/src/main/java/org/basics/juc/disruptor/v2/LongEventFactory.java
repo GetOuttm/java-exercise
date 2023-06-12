47ebbdbfda562fe079250a7e6a4556f19ee17ae3
GetOuttm <13313533256@163.com>
2023-06-12 13:21:06 +0800

    juc

---
+++ basics-exercise/basics/src/main/java/org/basics/juc/disruptor/v2/LongEventFactory.java
@@ -0,0 +1,10 @@
+package org.basics.juc.disruptor.v2;
+
+import com.lmax.disruptor.EventFactory;
+
+public class LongEventFactory implements EventFactory<LongEvent> {
+
+    public LongEvent newInstance() {
+        return new LongEvent();
+    }
+}
